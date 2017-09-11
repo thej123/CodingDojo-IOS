f87373df5e9be5b2638a5ade983ecabc091c13fa
Thej <thej@Juans-Mac-mini.local>
2017-09-11 13:11:56 -0700

    9/11/17 added hackathon zombie project along with my chicken modification

---
+++ tipster/tipster/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  tipster
+//
+//  Created by Thej on 9/8/17.
+//  Copyright © 2017 Thej. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func tipSlider(_ sender: UISlider) {
+    }
+    @IBAction func groupSizeSlider(_ sender: UISlider) {
+    }
+    
+    
+    
+    @IBAction func Button0(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button1(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button2(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button3(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button4(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button5(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button6(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button7(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button8(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button9(_ sender: UIButton) {
+    }
+    
+    @IBAction func Button_C(_ sender: UIButton) {
+    }
+    
+    @IBAction func ButtonDot(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
